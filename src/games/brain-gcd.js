d6a7bf2fe000d4c421e4e8a368d902ce2a1541cd
aydaver <Aydar.kalin@mail.ru>
2024-07-17 00:08:51 +0300

    make brain-gcd more simple and shorter

--- src/games/brain-gcd.js
+++ src/games/brain-gcd.js
@@ -1,26 +1,12 @@
 import getRandomInt from '../utils/getRandomInt.js';
+import pushCD from '../utils/pushCD.js';
 
 const run = () => {
   const randInt1 = getRandomInt(1, 100);
   const randInt2 = getRandomInt(1, 100);
   const quest = `${randInt1} ${randInt2}`;
-  const nums = [];
   let answer = 0;
-  if (randInt1 > randInt2) {
-    for (let i = 1; i <= randInt2; i += 1) {
-      if (randInt2 % i === 0 && randInt1 % i === 0) {
-        nums.push(i);
-      }
-    }
-  } else if (randInt2 > randInt1) {
-    for (let i = 1; i <= randInt1; i += 1) {
-      if (randInt2 % i === 0 && randInt1 % i === 0) {
-        nums.push(i);
-      }
-    }
-  } else {
-    answer = randInt1;
-  }
+  const nums = pushCD(randInt1, randInt2);
   answer = nums[nums.length - 1];
   answer = answer.toString();
   return [quest, answer];
