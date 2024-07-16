2dd68c753a10971d9d10fdcbfb75ac4936327c39
aydaver <Aydar.kalin@mail.ru>
2024-07-16 23:58:05 +0300

    make brain-gcd shorter

--- src/games/brain-gcd.js
+++ src/games/brain-gcd.js
@@ -4,51 +4,26 @@ const run = () => {
   const randInt1 = getRandomInt(1, 100);
   const randInt2 = getRandomInt(1, 100);
   const quest = `${randInt1} ${randInt2}`;
+  const nums = [];
   let answer = 0;
   if (randInt1 > randInt2) {
-    const nums = [];
     for (let i = 1; i <= randInt2; i += 1) {
-      nums.push(i);
-    }
-    const almostRightNums = [];
-    for (let i = 0; i <= nums.length; i += 1) {
-      if (randInt2 % nums[i] === 0) {
-        almostRightNums.push(nums[i]);
+      if (randInt2 % i === 0 && randInt1 % i === 0) {
+        nums.push(i);
       }
     }
-    const rightNums = [];
-    for (let i = 0; i <= almostRightNums.length; i += 1) {
-      if (randInt1 % almostRightNums[i] === 0) {
-        rightNums.push(almostRightNums[i]);
-      }
-    }
-    answer = rightNums[rightNums.length - 1];
   } else if (randInt2 > randInt1) {
-    const nums = [];
     for (let i = 1; i <= randInt1; i += 1) {
-      nums.push(i);
-    }
-    const almostRightNums = [];
-    for (let i = 0; i <= nums.length; i += 1) {
-      if (randInt1 % nums[i] === 0) {
-        almostRightNums.push(nums[i]);
+      if (randInt2 % i === 0 && randInt1 % i === 0) {
+        nums.push(i);
       }
     }
-    const rightNums = [];
-    for (let i = 0; i <= almostRightNums.length; i += 1) {
-      if (randInt2 % almostRightNums[i] === 0) {
-        rightNums.push(almostRightNums[i]);
-      }
-    }
-    answer = rightNums[rightNums.length - 1];
   } else {
     answer = randInt1;
   }
+  answer = nums[nums.length - 1];
   answer = answer.toString();
-
   return [quest, answer];
 };
-
 const rules = 'Find the greatest common divisor of given numbers.';
-
 export default { run, rules };
