380e233c1aff4654732ba88e2c8108e0cd040895
aydaver <Aydar.kalin@mail.ru>
2024-07-17 16:53:40 +0300

    make brain-gcd way to more simple and shorter

--- src/games/brain-gcd.js
+++ src/games/brain-gcd.js
@@ -1,15 +1,29 @@
 import getRandomInt from '../utils/getRandomInt.js';
-import pushCD from '../utils/pushCD.js';
 
 const run = () => {
   const randInt1 = getRandomInt(1, 100);
   const randInt2 = getRandomInt(1, 100);
   const quest = `${randInt1} ${randInt2}`;
   let answer = 0;
-  const nums = pushCD(randInt1, randInt2);
+  const nums = [];
+  let lowestInt = 0;
+  if (randInt1 > randInt2) {
+    lowestInt = randInt2;
+  } else if (randInt2 > randInt1) {
+    lowestInt = randInt1;
+  } else {
+    nums.push(randInt1);
+  }
+  for (let i = 1; i <= lowestInt; i += 1) {
+    if (randInt2 % i === 0 && randInt1 % i === 0) {
+      nums.push(i);
+    }
+  }
   answer = nums[nums.length - 1];
   answer = answer.toString();
   return [quest, answer];
 };
+
 const rules = 'Find the greatest common divisor of given numbers.';
+
 export default { run, rules };
