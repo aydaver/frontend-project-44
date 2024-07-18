71577e387b6b50d797bd753fe4a4ef63cf386644
aydaver <Aydar.kalin@mail.ru>
2024-07-18 22:03:48 +0300

    change brain-progression

--- src/games/brain-progression.js
+++ src/games/brain-progression.js
@@ -1,10 +1,10 @@
 import getRandomInt from '../utils/getRandomInt.js';
 
 const run = () => {
-  const randInt = getRandomInt(1, 20);
-  const progStep = getRandomInt(1, 8);
-  const progLength = getRandomInt(5, 15);
-  const dotsPosit = getRandomInt(5, progLength - 1);
+  const randInt = getRandomInt(1, 30);
+  const progStep = getRandomInt(1, 10);
+  const progLength = getRandomInt(6, 15);
+  const dotsPosit = getRandomInt(5, progLength);
   const quest = [randInt];
   for (let i = 0; i < progLength; i += 1) {
     quest.push(quest[i] + progStep);
