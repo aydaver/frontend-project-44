8883c464218522b71323d790d27760828a67f95d
aydaver <Aydar.kalin@mail.ru>
2024-07-18 21:27:27 +0300

    fix brain-progression

--- src/games/brain-progression.js
+++ src/games/brain-progression.js
@@ -4,7 +4,7 @@ const run = () => {
   const randInt = getRandomInt(1, 20);
   const progStep = getRandomInt(1, 8);
   const progLength = getRandomInt(5, 15);
-  const dotsPosit = getRandomInt(5, 15);
+  const dotsPosit = getRandomInt(5, progLength);
   const preQuest = [randInt];
   for (let i = 0; i < progLength; i += 1) {
     preQuest.push(preQuest[i] + progStep);
